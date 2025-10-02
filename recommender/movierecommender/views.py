cda3c810010f570849a2b33e8d80a824cb4908dd
sahil-313 <90836690+sahil-313@users.noreply.github.com>
2025-10-02 19:14:09 +0530

    Fix formatting in views.py

--- recommender/movierecommender/views.py
+++ recommender/movierecommender/views.py
@@ -10,20 +10,12 @@ def movie_recommendation_view(request):
 def generate_movies_context():
     context = {}
 
-    recommended_count = Movie.objects.filter(
-        recommended = True
-    ).count()
+    recommended_count = Movie.objects.filter(recommended = True).count()
 
     if recommended_count == 0:
-        movies = Movie.objects.filter(
-            watched = False
-        ).order_by("-vote_count")[:30]
+        movies = Movie.objects.filter(watched = False).order_by("-vote_count")[:30]
     else:
-        movies = Movie.objects.filter(
-            watched = False
-        ).filter(
-            recommended = True
-        ).order_by("-vote_count")[:30]
+        movies = Movie.objects.filter(watched = False).filter(recommended = True).order_by("-vote_count")[:30]
 
     context["movie_list"] = movies
     return context
