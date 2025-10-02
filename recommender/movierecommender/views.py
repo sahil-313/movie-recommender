966e8b1926536234aeccbad1ffc06586a77b6dc0
root <root@LAPTOP-UQIC8PGC.localdomain>
2025-10-02 18:07:59 +0530

    Remove comment

--- recommender/movierecommender/views.py
+++ recommender/movierecommender/views.py
@@ -2,7 +2,6 @@ from . import views
 from .models import Movie
 from django.shortcuts import render
 
-# HINT: Create a view to provide movie recommendations list for the HTML template
 def movie_recommendation_view(request):
     if request.method == "GET":
         # The context/data to be presented in the HTML template
