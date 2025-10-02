3d5401f85bd28e864b104b77b27f4590fe657be0
root <root@LAPTOP-UQIC8PGC.localdomain>
2025-10-02 17:15:18 +0530

    Update movie_recommendation_view()

--- recommender/movierecommender/views.py
+++ recommender/movierecommender/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 def movie_recommendation_view(request):
     if request.method == "GET":
         # The context/data to be presented in the HTML template
-        context = {}
+        context = generate_movies_context()
         # Render a HTML page with specified template and context
         return render(request, "movierecommender/movie_list.html", context)
     
